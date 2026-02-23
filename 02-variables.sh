ee605fa4c9c87e30f6931b360eed51bb9acf608e
Raveendra Meesala <meesala10@gmail.com>
2026-02-24 00:34:10 +0530

    message

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+
+person1=Ravi
+person2=vasu
+
+echo "Ravi : how are you vasu "
+echo "vasu : fine ravi and how about you"
+echo "Ravi : fine what are doing"
+echo "vasu :not daily common routune"
