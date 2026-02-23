33bf46c90a43941061e0dba9fa6dbda99054bcc1
Raveendra Meesala <meesala10@gmail.com>
2026-02-24 01:27:15 +0530

    message

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-MOVIES=("og" "harihara" "bn" "vk")
+person1=$1
+person2=$2
 
-echo "first movie is ${MOVIES[@]}"
+echo "$person1 : how are you $person2 "
+echo "$person2 : fine $person1 and how about you"
+echo "$person1 : fine what are doing"
+echo "$person2 :not daily common routune"
