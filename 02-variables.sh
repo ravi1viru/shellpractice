549618f0c1f43ff42e7ebccf06a149b4e7b056b2
Raveendra Meesala <meesala10@gmail.com>
2026-02-24 02:30:39 +0530

    message

--- 02-variables.sh
+++ 02-variables.sh
@@ -5,19 +5,28 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
    echo "Error :: istall with root user"
-   
+   exit 1
 else
    echo "you can install root user"
 
 fi
+dnf list installed mysql
 
-dnf install mysqlgfgg -y
+if [ $? -ne 0 ]
+then
+    echo "MySQL is not installed... going to install it"
+    dnf install mysql -y
 
 if [ $? -eq 0 ]
 then
    echo "mysql is installed status....SUCCESS"
 else
    echo "mysql is installed status....FAILURE"
-
+exit 1
 
 fi
+else
+    echo "MySQL is already installed... Nothing to do"
+    exit 1
+fi
+
