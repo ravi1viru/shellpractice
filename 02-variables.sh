84a9cd81f9bbf280076c79e24c2b0326c61168e7
Raveendra Meesala <meesala10@gmail.com>
2026-02-24 03:09:49 +0530

    message

--- 02-variables.sh
+++ 02-variables.sh
@@ -9,24 +9,42 @@ then
 else
    echo "you can install root user"
 
-fi
-dnf list installed mysql
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then
+        echo "Installing $2 is ... SUCCESS"
+    else
+        echo "Installing $2 is ... FAILURE"
+        exit 1
+    fi
+}
 
+dnf list installed mysql
 if [ $? -ne 0 ]
 then
     echo "MySQL is not installed... going to install it"
     dnf install mysql -y
-
-if [ $? -eq 0 ]
-then
-   echo "mysql is installed status....SUCCESS"
-else
-   echo "mysql is installed status....FAILURE"
-exit 1
-
-fi
+    VALIDATE $? "MySQL"
 else
     echo "MySQL is already installed... Nothing to do"
-    exit 1
 fi
 
+dnf list installed python3
+if [ $? -ne 0 ]
+then
+    echo "python3 is not installed... going to install it"
+    dnf install python3 -y
+    VALIDATE $? "python3"
+else
+    echo "python3 is already installed... Nothing to do"
+fi
+
+dnf list installed nginx
+if [ $? -ne 0 ]
+then
+    echo "nginx is not installed... going to install it"
+    dnf install nginx -y
+    VALIDATE $? "nginx"
+else
+    echo "nginx is already installed... Nothing to do"
+fi
