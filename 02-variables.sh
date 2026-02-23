59d7b2a2a04c25eee4f1713662c669c230795189
Raveendra Meesala <meesala10@gmail.com>
2026-02-24 00:51:35 +0530

    message

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-person1=$1
-person2=$2
-
-echo "$person1 : how are you $person2 "
-echo "$person2 : fine $person1 and how about you"
-echo "$person1 : fine what are doing"
-echo "$person2 :not daily common routune"
+echo "please enter the pin number"
+read PIN
+echo "Your $PIN number is"
