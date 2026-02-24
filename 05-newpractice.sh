19b86f6fe9bb69ca7302b69b60a1e1384600cca6
Raveendra Meesala <meesala10@gmail.com>
2026-02-25 01:17:17 +0530

    message

--- 05-newpractice.sh
+++ 05-newpractice.sh
@@ -4,7 +4,49 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]
 then
-   echo -e "/e[31m ERROR :: Please run the script with root user"
+   echo -e " ERROR :: Please run the script with root user"
+   exit 1
 else
    echo "code is run with the rootuser"
+fi
+
+VALIDATE(){
+
+    if [ $1 -eq 0 ]
+    then
+      echo "the software $2 is installed ... SUCCESS"
+    else
+      echo "the software $2 is installed ... FAILURE"
+      exit 1
+    fi
+}
+
+dnf list installed mysql
+if [ $? -ne 0]
+then 
+echo "please install the sql server"
+     dnf install mysql -y
+     VALIDATE() "MYSQL"
+else
+echo "already installed the sql server no need"
+fi
+
+dnf list installed python3
+if [ $? -ne 0]
+then 
+echo "please install the python server"
+     dnf install pthon3 -y
+     VALIDATE() "python3"
+else
+echo "already installed the python server no need"
+fi
+
+dnf list installed nginx
+if [ $? -ne 0]
+then 
+echo "please install the nginx server"
+     dnf install nginx -y
+     VALIDATE() "nginx"
+else
+echo "already installed the nginx server no need"
 fi
