7f9703c9c4d68f2f8b2471cca919e2f83786254f
Raveendra Meesala <meesala10@gmail.com>
2026-02-25 01:01:56 +0530

    message

--- 05-newpractice.sh
+++ 05-newpractice.sh
@@ -4,7 +4,7 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]
 then
-   echo -e "/e[31m ERROR :: Please run the script with root user /e[0m"
+   echo -e "/e[31m ERROR :: Please run the script with root user"
 else
    echo "code is run with the rootuser"
 fi
